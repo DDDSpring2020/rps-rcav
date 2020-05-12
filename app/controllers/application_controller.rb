4fc2a5603df50bf4cc1aa657716ca4fe32c61302
panmarq <panama.marquand@kellogg.northwestern.edu>
2020-05-12 22:22:18 +0000

    Setup Routes, controller, rock, scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+
+  def play_rock
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+  
+  def play_scissor
+    render({ :template => "game_templates/user_scissor.html.erb" })
+  end
+
+end 
+ 
 
   # Add your actions below this line
   # ================================
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "homepage" })
   
+  get("/rock", { :controller => "application", :action => "play_rock"})
+  
+  get("/paper", { :controller => "application", :action => "play_paper"})
+
+  get("/scissor", { :controller => "application", :action => "play_scissor"})
+
+end
 
   # Write your routes above this line
   # =================================
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-end
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
